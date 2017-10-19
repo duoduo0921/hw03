edb1324457a2ebafa72c15490b0442a1ba1f7bb9
duoduo0921 <wang.hand@husky.neu.edu>
2017-10-19 18:46:06 -0400

    cleared admin

--- priv/repo/migrations/20171019212206_addis_admin.exs
+++
@@ -1,9 +0,0 @@
-defmodule Microblog.Repo.Migrations.AddisAdmin do
-  use Ecto.Migration
-
-  def change do
-  alter table("users") do
-      add :is_admin?, :boolean, null: false, default: false
-end
-  end
-end
