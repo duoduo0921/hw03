5fabd4b0a1fca95b3d5f42470fc04b3b81d065f5
duoduo0921 <wang.hand@husky.neu.edu>
2017-10-19 18:02:09 -0400

    adds url

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :microblog, MicroblogWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "microblog.handlek.com", port: 8000],
+  url: [host: "microblog.handlek0921.com", port: 8000],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
